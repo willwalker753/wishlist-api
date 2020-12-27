5b013b0936975b284c80fe00e017c7b0a53eeb38
Will Walker <williamwalker753@gmail.com>
2020-12-27 15:10:41 -0600

    url metadata feature

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const bodyParser = require('body-parser');
+const urlMetadata = require('url-metadata')
 
 const app = express()
 
@@ -11,13 +13,24 @@ const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 
+app.use(bodyParser.json());
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
-app.get('/', (req, res) => {
-   res.send('Hello, world!')
-})
+app.post('/', async (req, res) => {
+    let url = req.body.url;
+    let data;
+    await urlMetadata(url).then(
+    function (metadata) { // success handler
+        console.log(metadata);
+        data = metadata;
+    },
+    function (error) { // failure handler
+        console.log(error)
+    })
+    res.send(data);
+});
 
 app.use(function errorHandler(error, req, res, next) {
     let response
